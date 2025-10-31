a22838eaa3596b91943ec3b5cd9726993ee52c10
gomes-milindu <gomesmilindu@gmail.com>
2025-10-31 11:57:12 +0530

    starting use State Hook 😁

--- pages/testPage.jsx
+++ pages/testPage.jsx
@@ -1,8 +1,36 @@
-import  { Routes, Route } from 'react-router-dom'
+import { useState } from "react"
+
 export default function TestPage(){
+    
+    const[count,setCount] =  useState(100)   // count variable  b - function
+    
     return(
-        <div className = "w-full h-[100px] bg-accent">
-            <h1>Test Page</h1>
+        <div className = "w-full h-full bg-accent">
+            <div className='w-full h-full flex justify-center items-center' >
+                <div className='w-[500px] h-[500px] bg-white '>
+                    <button onClick={
+                        ()=>{
+                            console.log("clicked")
+                            setCount(count + 1)
+                        }
+                    } className='w-[100px] h-[40px] bg-amber-200 rounded-lg'>+</button>
+                </div>
+
+                <span className='w-[40px] h-fit bg-amber-700'>
+                    {count}
+                </span>
+                <div className='w-[500px] h-[500px] bg-white '>
+                    <button className='w-[100px] h-[40px] bg-amber-200 rounded-lg' onClick={
+                        ()=>{
+                            console.log("- is Clicked")
+                            setCount(count-1)
+                        }
+                    }>-</button>
+                </div>
+            </div>
         </div>
     )
 }
+
+// set count - variable eke valure eka wenas wenawa
+               // page eka uda in pahalata run wenawa
